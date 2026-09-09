daa305b65ac43000fe10ba87719336798a1a13b9
Olivia Nicolle <olivia.nicolle.72@gmail.com>
2026-09-09 05:54:02 -0300

    Add files via upload

--- game/game.js
+++ game/game.js
@@ -1,11 +1,6 @@
 const playButton = document.getElementById('play');
 const wrapper = document.getElementById('game-wrapper');
-const luaLinha = document.getElementById('lua-linha');
 
 playButton.addEventListener('click', function() {
     wrapper.classList.add('sumir');
-
-    setTimeout(function() {
-        luaLinha.classList.add('expandir-linha');
-    }, 200);
 });
